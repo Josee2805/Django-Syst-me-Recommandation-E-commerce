0ea83c6c8668b67360768a3cc72d9310fe54a371
Aissatou150 <aissatoug15@gmail.com>
2026-03-26 22:05:19 +0000

    Mise à jour

--- ecommerce_reco/settings.py
+++ ecommerce_reco/settings.py
@@ -62,10 +62,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce_reco.wsgi.application'
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+#         conn_max_age=600,
+#     )
+# }
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}")     ,
         conn_max_age=600,
+        ssl_require=True, 
     )
 }
 
